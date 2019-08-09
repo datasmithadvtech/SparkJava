a233f2e97134d956490ae04fb069c7ad168cbd4e
prashantnkhunt <prashant.n.khunt@gmail.com>
2019-08-09 19:59:50 +0530

    Minor changes related to map in the code

--- SparkRDDOperations/src/main/java/com/javadeveloperzone/spark/UnionExample.java
+++ SparkRDDOperations/src/main/java/com/javadeveloperzone/spark/UnionExample.java
@@ -7,8 +7,11 @@ import org.apache.spark.api.java.JavaSparkContext;
 import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
 
 import java.util.Arrays;
+import java.util.List;
 
 public class UnionExample {
+
+    private static List<String> ArrayList;
 
     public static void main(String[] args){
 
@@ -26,7 +29,22 @@ public class UnionExample {
 
         JavaRDD<String> likeToLearn = likes.union(learn);
 
-        System.out.println("I like "+likeToLearn.collect().toString());
+        List<String> result = likeToLearn.collect();
+
+        //Learning::2
+        System.out.println("Learning::"+learn.count());
+
+        //Prints I like [Java, Spark, Scala]
+        System.out.println("I like "+result.toString());
+
+        String[]  newlyLearningSkills = {"Elastic Search","Spring Boot"};
+
+        JavaRDD<String> learningSkills = learn.union(javaSparkContext.parallelize(Arrays.asList(newlyLearningSkills)));
+
+//        learningSkills::[Spark, Scala, Elastic Search, Spring Boot]
+        System.out.print("learningSkills::"+learningSkills.collect().toString());
+
+
 
         javaSparkContext.stop();
 
